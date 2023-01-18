2944d5281dd61a0a5b58d0b7f8ccf73e5f96b494
amansoni <amansoni2041999@gmail.com>
2023-01-18 16:13:11 +0530

    performing Stackandqueue program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue2022
+{
+    internal class LinkedListStack
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the stack element");
+            LinkedListStack stacklist = new LinkedListStack();
+            stacklist.Push(70);
+            stacklist.Push(30);
+            stacklist.Push(56);
+            stacklist.Display();
+            Console.ReadLine();
+        }
+    }
+}
